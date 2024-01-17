ea9cc38c63aa2fe137c72e4e3d7b00381cd97aa5
rayssassousa20 <rayssbtx1@gmail.com>
2024-01-17 15:20:14 -0300

    training how to work with classes and objects

--- testes.js
+++ testes.js
@@ -1,1 +1,17 @@
-console.log("ola");
+class userInformation{
+    constructor(name, age, introduction){
+        this.name = name
+        this.age = age
+        this.introduction = introduction
+    }
+
+    display(){
+        console.log(`username: ${this.name}`)
+        console.log(`age: ${this.age}`)
+        console.log(`resume: ${this.introduction}`)
+    }
+}
+
+let displayUserInformation = new userInformation("Rayssa", 18, "Hi!")
+
+displayUserInformation.display()
